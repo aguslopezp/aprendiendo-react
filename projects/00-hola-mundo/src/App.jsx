991ee2e6fad5bc113d83383830f8d91f685d910f
aguslopezp <agustinlopezprieto@gmail.com>
2023-01-23 00:45:02 -0300

    Comienzo primer proyecto

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,20 @@
+export function App () {
+
+    return (
+        <article>
+            <header>
+                <img alt="El avatar de carita" src="https://unavatar.io/instagram/willsmith"/>
+                <div>
+                    <strong>Will Smith</strong>
+                    <span>@willsmith</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article> 
+    )
+}
